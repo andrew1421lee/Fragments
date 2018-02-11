28be25062df1452763c22f2d699b5701c68f1528
Andrew Lee <andrew1421lee@gmail.com>
2018-02-10 20:45:16 -0500

    map styled and locked on location

--- app/src/main/java/com/lee/etirps/fragments/MapsActivity.java
+++ app/src/main/java/com/lee/etirps/fragments/MapsActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -22,19 +23,26 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
+import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.VisibleRegion;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsActivity extends FragmentActivity implements OnMyLocationButtonClickListener, OnMyLocationClickListener, LocationListener, OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMyLocationButtonClickListener, OnMyLocationClickListener, LocationListener, OnMapReadyCallback, OnCameraIdleListener {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient myLocation;
     private LocationCallback mLocationCallback;
     private UiSettings mUiSettings;
+    private double viewWidth = 0.002452544867992401;
+    private double viewHeight = 0.002948395655657521;
+    //private LatLngBounds CenterBound;
     //private LocationManager locationManager;
     //private static final long MIN_TIME = 200;
     //private static final float MIN_DISTANCE = 1;
@@ -57,6 +65,11 @@ public class MapsActivity extends FragmentActivity implements OnMyLocationButton
                 LatLng latLng = new LatLng(last.getLatitude(), last.getLongitude());
                 CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 18);
                 mMap.animateCamera(cameraUpdate);
+
+                LatLng one = new LatLng(latLng.latitude - (viewHeight/2), latLng.longitude - (viewWidth /2));
+                LatLng two = new LatLng(latLng.latitude + (viewHeight/2),latLng.longitude + (viewWidth/2));
+                LatLngBounds box = new LatLngBounds(one, two);
+                mMap.setLatLngBoundsForCameraTarget(box);
             }
         };
     }
@@ -77,16 +90,24 @@ public class MapsActivity extends FragmentActivity implements OnMyLocationButton
         enableMyLocation();
         mMap.setOnMyLocationButtonClickListener(this);
         mMap.setOnMyLocationClickListener(this);
+        mMap.setOnCameraIdleListener(this);
         mMap.addMarker(new MarkerOptions().position(new LatLng(42.109015, -75.946749)).title("My Home"));
+
+        //boolean styled = mMap.setMapStyle(new MapStyleOptions(getResources().getString(R.string.lite_json)));
+
+        boolean styled = mMap.setMapStyle( MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style));
+
+        //Log.v("bugsa",getResources().getString(R.string.style_json));
 
         mUiSettings = mMap.getUiSettings();
         mUiSettings.setMapToolbarEnabled(false);
         mUiSettings.setZoomGesturesEnabled(false);
-        mUiSettings.setScrollGesturesEnabled(false);
         mUiSettings.setZoomControlsEnabled(false);
         mUiSettings.setRotateGesturesEnabled(false);
+        mUiSettings.setTiltGesturesEnabled(false);
+        mMap.setBuildingsEnabled(false);
 
-        mUiSettings.setTiltGesturesEnabled(true);
+        mUiSettings.setScrollGesturesEnabled(true);
 
         try{
             myLocation.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
@@ -95,6 +116,12 @@ public class MapsActivity extends FragmentActivity implements OnMyLocationButton
                     LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                     CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 18);
                     mMap.animateCamera(cameraUpdate);
+
+                    LatLng one = new LatLng(latLng.latitude - (viewHeight/2), latLng.longitude - (viewWidth /2));
+                    LatLng two = new LatLng(latLng.latitude + (viewHeight/2),latLng.longitude + (viewWidth/2));
+                    LatLngBounds box = new LatLngBounds(one, two);
+                    mMap.setLatLngBoundsForCameraTarget(box);
+
                 }
             });
             myLocation.requestLocationUpdates(new LocationRequest().setInterval(200).setSmallestDisplacement(5),mLocationCallback,null);
@@ -108,6 +135,15 @@ public class MapsActivity extends FragmentActivity implements OnMyLocationButton
     @Override
     public void onMyLocationClick(@NonNull Location location) {
         Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+
+
+        VisibleRegion viewPort = mMap.getProjection().getVisibleRegion();
+        mMap.addMarker(new MarkerOptions().position(viewPort.farLeft));
+        mMap.addMarker(new MarkerOptions().position(viewPort.farRight));
+
+        Log.v("width", "test " + (viewPort.farLeft.longitude - viewPort.nearRight.longitude));
+        Log.v("height", "test " + (viewPort.farLeft.latitude - viewPort.nearRight.latitude));
+
         //mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())));
     }
 
@@ -117,6 +153,12 @@ public class MapsActivity extends FragmentActivity implements OnMyLocationButton
         //Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 18);
         mMap.animateCamera(cameraUpdate);
+
+        /*
+        VisibleRegion viewPort = mMap.getProjection().getVisibleRegion();
+        CenterBound = viewPort.latLngBounds;
+        mMap.setLatLngBoundsForCameraTarget(CenterBound);*/
+
         //Location lastLocation = location;
         //mMap.addCircle(new CircleOptions().center(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude())).radius(5));
         //mMap.addMarker(new MarkerOptions().position(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude())));
@@ -129,6 +171,13 @@ public class MapsActivity extends FragmentActivity implements OnMyLocationButton
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
+    }
+
+    @Override
+    public void onCameraIdle(){
+        Toast.makeText(this, "IDLE", Toast.LENGTH_SHORT).show();
+
+
     }
 
     private void enableMyLocation() {
